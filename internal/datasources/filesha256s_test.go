2118fcdd895bb74da55d21f01f93d764cb9ccc30
Andreas Bergmeier <andreas.bergmeier@otto.de>
2021-12-08 10:54:54 +0100

    Test file order to be of no consequence.
    Produces same hash.

--- internal/datasources/filesha256s_test.go
+++ internal/datasources/filesha256s_test.go
@@ -16,4 +16,15 @@ func TestGenerateSha256OfFiles(t *testing.T) {
 	if hash != "09fd542731bded7a10813ce214447b685bfbc247546f1c4f363c9282c7d3a025" {
 		t.Fatal("Expected hash 09fd542731bded7a10813ce214447b685bfbc247546f1c4f363c9282c7d3a025", "Received hash:", hash)
 	}
+
+	hash, err = generateSha256OfFiles([]string{
+		"testdata/foo.bar",
+		"testdata/testme.tst",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if hash != "09fd542731bded7a10813ce214447b685bfbc247546f1c4f363c9282c7d3a025" {
+		t.Fatal("Expected hash 09fd542731bded7a10813ce214447b685bfbc247546f1c4f363c9282c7d3a025", "Received hash:", hash)
+	}
 }
